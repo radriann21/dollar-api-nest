522830b8329d5b8c4f51402526a158cfb5ba8d2e
Adrian Rangel <adrianrangelargu@gmail.com>
2026-02-21 18:41:07 -0400

    feat: added redis for cache managing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { createKeyv } from '@keyv/redis';
+import { CacheModule } from '@nestjs/cache-manager';
+
+// Modules
 import { ScrapperModule } from './scrapper/scrapper.module';
 import { TasksModule } from './tasks/tasks.module';
 import { ScheduleModule } from '@nestjs/schedule';
-import { ConfigModule } from '@nestjs/config';
 import { RatesModule } from './rates/rates.module';
 import { AnalyticsModule } from './analytics/analytics.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.registerAsync({
+      useFactory: () => ({
+        stores: [createKeyv('redis://localhost:6379')],
+        ttl: 60 * 60 * 1000,
+      }),
+      isGlobal: true,
+    }),
     ScrapperModule,
     TasksModule,
     ScheduleModule.forRoot(),
